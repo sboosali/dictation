94c38aa1660495aaadd5b019decb2e7cae400218
Spiros Boosalis <SamBoosalis@gmail.com>
2018-08-13 01:36:33 -0700

    add dependencies

--- dictation-server/sources/Dictation/Server/API.hs
+++ dictation-server/sources/Dictation/Server/API.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 --------------------------------------------------
 
@@ -6,6 +7,7 @@
 
 
 -}
+
 module Dictation.Server.API where
 
 --------------------------------------------------
@@ -14,25 +16,52 @@ import Prelude_dictation_server
 
 --------------------------------------------------
 
+import qualified "servant-server" Servant     as S
+import qualified "servant"        Servant.API as S
+
+--------------------------------------------------
+
+import qualified "sqlite-simple" Database.SQLite.Simple as SQL
+
+--------------------------------------------------
+
+import qualified "http-client" Network.HTTP.Client      as HTTP (newManager, defaultManagerSettings)
+import qualified "warp"        Network.Wai.Handler.Warp as Warp
+
+--------------------------------------------------
+
+import qualified "text" Data.Text as T
+
+--------------------------------------------------
+
+import qualified "time" Data.Time as Time (UTCTime)
+
+--------------------------------------------------
+
+import qualified "base" Control.Concurrent as Concurrent
+
+--------------------------------------------------
+
+-- import Control.Exception      (bracket)
+-- import Control.Monad.IO.Class
+
+--------------------------------------------------
+--------------------------------------------------
+
 
 
 --------------------------------------------------
 
 
---------------------------------------------------
---------------------------------------------------
-
 
 
 --------------------------------------------------
-
-
-
-
+-- NOTES -----------------------------------------
 --------------------------------------------------
 
-
-
-
+-- See
+--     - https://haskell-servant.readthedocs.io/en/stable/cookbook/db-sqlite-simple/DBConnection.html
+--     - 
+--
 
 --------------------------------------------------
