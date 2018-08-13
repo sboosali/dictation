b51bf8ef9c515c5b9b1b179dc497cf92a5ef116e
Spiros Boosalis <SamBoosalis@gmail.com>
2018-08-13 10:07:44 -0700

    dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,19 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; ((dante-mode
+;;   . ((dante-project-root      . "")
+;;      (dante-repl-command-line . ""))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; e.g.
+;;
+;;     ((nil . ((indent-tabs-mode . t)
+;;              (fill-column . 80)))
+;;      (c-mode . ((c-file-style . "BSD")
+;;                 (subdirs . nil)))
+;;      ("src/imported"
+;;       . ((nil . ((change-log-default-name
+;;                   . "ChangeLog.local"))))))
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
